7932007888624279796f08eedf1b48e3e2bfd7c8
alex <gw11balsilliealex@glow.sch.uk>
2026-08-21 11:57:12 +0100

    Boot directly into the simplified workspace

--- js/boot.js
+++ js/boot.js
@@ -1,1 +1,12 @@
-init();
+document.body.classList.remove('startup-mode');
+
+const uiStylesheet=document.createElement('link');
+uiStylesheet.rel='stylesheet';
+uiStylesheet.href='css/ui.css';
+document.head.appendChild(uiStylesheet);
+
+const uiScript=document.createElement('script');
+uiScript.src='js/ui.js';
+uiScript.onload=()=>init();
+uiScript.onerror=()=>init();
+document.head.appendChild(uiScript);
